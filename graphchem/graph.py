104aa110e233e935ff66be49a8682b9610761502
tjkessler <travis.j.kessler@gmail.com>
2019-01-17 00:52:55 -0500

    Groundwork for applying a transition function to each atom

--- graphchem/graph.py
+++ graphchem/graph.py
@@ -8,6 +8,7 @@
 
 # Stdlib imports
 from re import compile
+from statistics import mean, median, stdev
 
 # Vector representation for each atom
 ATOMS = {'C': [1, 0], 'O': [0, 1]}
@@ -34,21 +35,22 @@ BOND_NAMES = {
 LINK = compile(r'\d')
 
 
-class _Node:
+class _Atom:
 
-    def __init__(self, id, atom, branch_lvl):
-        '''Node object: represents an atom and its connections/bonds
+    def __init__(self, id, char, branch_lvl):
+        '''Atom object: represents an atom and its connections/bonds
 
         Args:
             id (int): identifier for the atom
-            atom (str): symbol for the atom
+            char (str): symbol for the atom
             branch_lvl (int): used by Graph._connect to determine branch
                 connections
         '''
 
         self.connections = []
+        self.state = 0
         self._id = id
-        self._atom = atom
+        self._char = char
         self._bonds = []
         self._branch_lvl = branch_lvl
         self._link = None
@@ -73,7 +75,7 @@ class _Node:
                 number of disconnected bonds)
         '''
 
-        packed = [a for a in ATOMS[self._atom]]
+        packed = [a for a in ATOMS[self._char]]
         packed.extend([b for b in self._bonds])
         return tuple(packed)
 
@@ -102,7 +104,7 @@ class Graph:
                     char
                 ))
         self.smiles = smiles
-        self.nodes = []
+        self.atoms = []
         branch_lvl = 0
         bond_type = BONDS['-']
         bond_name = BOND_NAMES['-']
@@ -123,45 +125,46 @@ class Graph:
                 bond_name = BOND_NAMES[char]
             elif LINK.match(char) is not None:
                 _offset += 1
-                self.nodes[-1]._link = int(char)
-                for li, node in enumerate(self.nodes[0: -1]):
-                    if node._link == int(char):
-                        node.connections.append(
+                self.atoms[-1]._link = int(char)
+                for li, atom in enumerate(self.atoms[0: -1]):
+                    if atom._link == int(char):
+                        atom.connections.append(
                             (idx - _offset, bond_type, bond_name)
                         )
-                        self.nodes[-1].connections.append(
+                        self.atoms[-1].connections.append(
                             (li, bond_type, bond_name)
                         )
                         break
             else:
-                new_node = _Node(idx - _offset, char, branch_lvl)
+                new_atom = _Atom(idx - _offset, char, branch_lvl)
                 if idx > 0:
-                    for i in range(1, len(self.nodes) + 1):
-                        if self.nodes[-1 * i]._branch_lvl == branch_lvl:
+                    for i in range(1, len(self.atoms) + 1):
+                        if self.atoms[-1 * i]._branch_lvl == branch_lvl:
                             if _new_branch:
                                 continue
                             else:
-                                self.nodes[-1 * i].connections.append(
+                                self.atoms[-1 * i].connections.append(
                                     (idx - _offset, bond_type, bond_name)
                                 )
-                                new_node.connections.append(
-                                    (len(self.nodes) - i, bond_type, bond_name)
+                                new_atom.connections.append(
+                                    (len(self.atoms) - i, bond_type, bond_name)
                                 )
                                 break
-                        elif self.nodes[-1 * i]._branch_lvl < branch_lvl:
-                            self.nodes[-1 * i].connections.append(
+                        elif self.atoms[-1 * i]._branch_lvl < branch_lvl:
+                            self.atoms[-1 * i].connections.append(
                                 (idx - _offset, bond_type, bond_name)
                             )
-                            new_node.connections.append(
-                                (len(self.nodes) - i, bond_type, bond_name)
+                            new_atom.connections.append(
+                                (len(self.atoms) - i, bond_type, bond_name)
                             )
                             _new_branch = False
                             break
-                self.nodes.append(new_node)
+                self.atoms.append(new_atom)
                 bond_type = BONDS['-']
                 bond_name = BOND_NAMES['-']
-        for node in self.nodes:
-            node.initialize()
+        for atom in self.atoms:
+            atom.initialize()
+        self.__prev_feeds = []
 
     def __repr__(self):
         '''Returns graph representation, with each atom's ID, atom symbol and
@@ -169,11 +172,11 @@ class Graph:
         '''
 
         r = 'ID\tAtom\tConnections\n'
-        for node in self.nodes:
+        for atom in self.atoms:
             r += '{}\t{}\t{}\n'.format(
-                node._id,
-                node._atom,
-                [(c[0], c[2]) for c in node.connections]
+                atom._id,
+                atom._char,
+                [(c[0], c[2]) for c in atom.connections]
             )
         return r
 
@@ -186,4 +189,97 @@ class Graph:
             disconnected bonds)
         '''
 
-        return [n.pack() for n in self.nodes]
+        return [a.pack() for a in self.atoms]
+
+    @property
+    def states(self):
+        '''Returns:
+            tuple: states of all atoms
+        '''
+
+        return tuple([a.state for a in self.atoms])
+
+    @property
+    def current_repr(self):
+        '''Returns:
+            tuple: current transition function feed lists for all atoms
+        '''
+
+        return [self._get_feed(a) for a in self.atoms]
+
+    @property
+    def prev_repr(self):
+        '''Returns:
+            tuple: previous transition function feed lists for all atoms (t-1)
+        '''
+
+        return self.__prev_feeds
+
+    def propagate(self, transition_fn):
+        '''Updates the states of all atoms using supplied function
+
+        Args:
+            transition_fn (callable): function to perform operation
+        '''
+
+        if not callable(transition_fn):
+            raise ReferenceError('Supplied `transition_fn` not callable')
+
+        new_states = []
+        self.__prev_feeds = []
+
+        for atom in self.atoms:
+            f = self._get_feed(atom)
+            self.__prev_feeds.append(f)
+            new_states.append(transition_fn(f))
+
+        for idx, atom in enumerate(self.atoms):
+            atom.state = new_states[idx]
+
+    def _get_feed(self, atom):
+        '''Obtains transition function feed lists for supplied atom
+
+        Args:
+            atom (_Atom): atom object
+        '''
+
+        neighbor_atoms = [self.atoms[a[0]] for a in atom.connections]
+        neighbor_states = [a.state for a in neighbor_atoms]
+        neighbor_repr = [a.pack() for a in neighbor_atoms]
+        return self._stack(neighbor_states) + self._vert_stack(neighbor_repr)
+
+    @staticmethod
+    def _stack(it):
+        '''Computes a "stack" from items in supplied iterable
+
+        Args:
+            it (iterable): iterable values, int or float
+        '''
+
+        if len(it) < 2:
+            sd = 0
+        else:
+            sd = stdev(it)
+        return (
+            min(it),
+            max(it),
+            mean(it),
+            sd
+        )
+
+    def _vert_stack(self, it):
+        '''Creates "stacks" for each iterable item's values using all items in
+            the iterable
+
+        Args:
+            it (iterable): iterable of iterables, where each sub-iterable is
+                of equal length and is populated with ints or floats
+        '''
+
+        stacks = []
+        for i in range(len(it[0])):
+            stacks.append(self._stack([s[i] for s in it]))
+        stack = []
+        for s in stacks:
+            stack.extend(s)
+        return tuple(stack)
