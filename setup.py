2f3cbc63bb038ebb3711433c8b767363236c6bd5
tjkessler <travis.j.kessler@gmail.com>
2019-11-21 18:58:03 -0500

    Update to version, dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='graphchem',
-    version='0.2.0',
+    version='0.3.0',
     description='Graph representations of molecules derived from SMILES'
     ' strings',
     url='http://github.com/tjkessler/graphchem',
@@ -10,6 +10,6 @@ setup(
     author_email='travis.j.kessler@gmail.com',
     license='MIT',
     packages=['graphchem'],
-    install_requires=[],
+    install_requires=['torch==1.3.1'],
     zip_safe=False
 )
