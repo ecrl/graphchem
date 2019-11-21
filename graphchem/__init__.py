72fe766fde20b157b80e5272d817c4b666c66311
tjkessler <travis.j.kessler@gmail.com>
2019-11-21 18:57:51 -0500

    Update to version, imports

--- graphchem/__init__.py
+++ graphchem/__init__.py
@@ -1,5 +1,6 @@
+from graphchem.atom import Atom
 from graphchem.molecule import Molecule
-import graphchem.atom
+from graphchem.gnn import MessagePassingNet, GNN
 import graphchem.atom_features
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Travis Kessler <travis.j.kessler@gmail.com>'
