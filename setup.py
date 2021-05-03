86ce44bea7cc30e5b360417c7c7196fdbbb902d1
Travis Kessler <travis.j.kessler@gmail.com>
2021-05-03 18:18:02 -0400

    Version bump: 1.0.0 -> 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphchem',
-    version='1.0.0',
+    version='1.1.0',
     description='Graph-based machine learning for chemical property prediction',
     url='https://github.com/ecrl/graphchem',
     author='Travis Kessler',
